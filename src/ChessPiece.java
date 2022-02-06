d0eff28ca892f5eea5f9354e9955632aa91ebcf0
Daniyar <mdaniyar1995@gmail.com>
2022-02-06 21:29:36 +0300

    Initial commit

---
+++ src/ChessPiece.java
@@ -0,0 +1,26 @@
+public abstract class ChessPiece {
+    String color;
+    boolean check = true;
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+
+    public abstract String getColor();
+
+    public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+
+    public abstract String getSymbol();
+
+    protected boolean chekPos(int pos) {
+        return pos >= 0 && pos <= 7;
+    }
+
+    protected int getMax(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    protected int getMin(int a, int b) {
+        return Math.min(a, b);
+    }
+}
